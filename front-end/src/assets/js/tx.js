d49dfcc3c266827787f4030aa4734532b5e893ce
anxin <anxin@anxins-MacBook-Pro.local>
2019-03-04 18:55:47 +0800

    plot and register submit

--- front-end/src/assets/js/tx.js
+++ front-end/src/assets/js/tx.js
@@ -3,41 +3,41 @@ import {Accounts} from 'web3-eth-accounts';
 // const coder = new AbiCoder();
 const accounts = new Accounts('ws://127.0.0.1:8650');
 
-// function add0x (input) {
-//     if (typeof(input) !== 'string') {
-//         return input;
-//     }
-//     if (input.length < 2 || input.slice(0,2) !== '0x') {
-//         return '0x' + input;
-//     }
-//     return input;
-// }
+function add0x (input) {
+    if (typeof(input) !== 'string') {
+        return input;
+    }
+    if (input.length < 2 || input.slice(0,2) !== '0x') {
+        return '0x' + input;
+    }
+    return input;
+}
 
-// function assembleFunction (jsonInterface, args) {
-//     // txObject contains gasPrice, gasLimit, nonce, to, value
-//     // let jsonInterface = {
-//     //     "name": "test",
-//     //     "type": "function",
-//     //     "inputs": [
-//     //         {
-//     //             "name": "c",
-//     //             "type": "int256"
-//     //         },
-//     //         {
-//     //             "name": "b",
-//     //             "type": "int256"
-//     //         }
-//     //     ],
-//     // };
-//
-//     let txData = coder.encodeFunctionCall(jsonInterface, args);
-//
-//     return txData
-// }
+function assembleFunction (jsonInterface, args) {
+    // txObject contains gasPrice, gasLimit, nonce, to, value
+    // let jsonInterface = {
+    //     "name": "test",
+    //     "type": "function",
+    //     "inputs": [
+    //         {
+    //             "name": "c",
+    //             "type": "int256"
+    //         },
+    //         {
+    //             "name": "b",
+    //             "type": "int256"
+    //         }
+    //     ],
+    // };
 
-// function signTx(tx, privateKey) {
-//     return accounts.signTransaction(tx,privateKey)
-// }
+    let txData = coder.encodeFunctionCall(jsonInterface, args);
+
+    return txData
+}
+
+function signTx(tx, privateKey) {
+    return accounts.signTransaction(tx,privateKey)
+}
 
 const createAccount = ()=> {
     let wallet =  accounts.create();
